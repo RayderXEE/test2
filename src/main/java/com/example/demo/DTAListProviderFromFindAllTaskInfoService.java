1c264d5da837c8643b4106c4aa9240e5266767de
RayderXEE <rayder123@gmail.com>
2022-11-03 11:31:08 +0300

    rename bat files add error report

--- src/main/java/com/example/demo/DTAListProviderFromFindAllTaskInfoService.java
+++ src/main/java/com/example/demo/DTAListProviderFromFindAllTaskInfoService.java
@@ -22,14 +22,20 @@ public class DTAListProviderFromFindAllTaskInfoService {
 
     private StarlingService starlingService;
 
-    @SneakyThrows
     @GetMapping("dtalist")
     public String getDTAList() {
-        String list = starlingService.doRequest(
-                "http://app-ru.huawei.com/hq_Russia_eurasia/services/api/dynamic/workflow/findalltaskinfo",
-                "{\"page\":1,\"pageSize\":500,\"userCode\":\"f84271284\",\"taskStatus\":\"0\",\"businessParams\":{},\"queryParams\":{\"currentHandler\":\"rWX357392\",\"createdDt\":{\"OPERATOR\":\"BETWEEN\",\"OPERAND\":[\"2022-10-21\",\"2100-12-31\"]},\"createdDtStart\":\"2022-10-21\",\"createdDtEnd\":\"2100-12-31\"}}");
-        ObjectMapper mapper = new ObjectMapper();
-        JsonNode jsonNodeList = mapper.readValue(list, JsonNode.class).get("tasks");
-        return jsonNodeList.toString();
+        try {
+            String list = starlingService.doRequest(
+                    "http://app-ru.huawei.com/hq_Russia_eurasia/services/api/dynamic/workflow/findalltaskinfo",
+                    "{\"page\":1,\"pageSize\":500,\"userCode\":\"f84271284\",\"taskStatus\":\"0\",\"businessParams\":{},\"queryParams\":{\"currentHandler\":\"rWX357392\",\"createdDt\":{\"OPERATOR\":\"BETWEEN\",\"OPERAND\":[\"2022-10-21\",\"2100-12-31\"]},\"createdDtStart\":\"2022-10-21\",\"createdDtEnd\":\"2100-12-31\"}}");
+            ObjectMapper mapper = new ObjectMapper();
+            JsonNode jsonNodeList = mapper.readValue(list, JsonNode.class).get("tasks");
+
+            return jsonNodeList.toString();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return e.toString();
+        }
+
     }
 }
